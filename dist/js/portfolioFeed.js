90b85dbd53a2d7fead24b7bf77465f8467f7a77e
Emilio Borraz <emilioborraz@gmail.com>
2018-12-11 15:32:30 -0500

    #comment Adding axios as http client and getting page content from an endpoint.

--- dist/js/portfolioFeed.js
+++ dist/js/portfolioFeed.js
@@ -1,6 +1,5 @@
-// Data (@todo to fetch the data from the server and use pre render data sent in the HTML template?)
-var wallElementsJsonData = '[{  "text": "My top artist on #Spotify2018 ...... #daftpunk https://t.co/OHv5G4mJqt",    "description": "",  "thumbnail": "https://pbs.twimg.com/media/Dtv4w7SXQAUXat8.jpg:thumb",    "created_at": "Thu Dec 06 16:54:28 +0000 2018", "resourceLink": "https://twitter.com/emilioborraz/status/1070723156455899136",  "id": "1070723156455899136",    "source": "twitter"},{  "text": "Me canso ganso :)",    "description": "",  "thumbnail": "",    "created_at": "Sat Dec 01 18:03:09 +0000 2018", "resourceLink": "https://twitter.com/emilioborraz/status/1068928503591182339",  "id": "1068928503591182339",    "source": "twitter"}, { "text": "Toma de posesión de @lopezobrador_  (via @brozoxmiswebs en @airelibre_fm ) #mexico",   "description": "",  "thumbnail": "",    "created_at": "Sat Dec 01 16:50:11 +0000 2018", "resourceLink": "https://twitter.com/emilioborraz/status/1068910140655583233",  "id": "1068910140655583233",    "source": "twitter"}, { "text": "#time We have to see more of each other https://t.co/ubBWvQpb4d",  "description": "",  "thumbnail": "",    "created_at": "Wed Nov 21 15:43:52 +0000 2018", "resourceLink": "https://twitter.com/emilioborraz/status/1065269571412770816",  "id": "1065269571412770816",    "source": "twitter"}, { "text": "Distracted #driving https://t.co/g6bnEz7JMH",  "description": "",  "thumbnail": "",    "created_at": "Wed Nov 28 04:52:45 +0000 2018", "resourceLink": "https://twitter.com/emilioborraz/status/1067642430370004992",  "id": "1067642430370004992",    "source": "twitter"}, { "text": "#JS ecosystem 2018 https://t.co/cFuqFCD9GD",   "description": "",  "thumbnail": "",    "created_at": "Sun Nov 25 17:23:42 +0000 2018", "resourceLink": "https://twitter.com/emilioborraz/status/1066744245904654336",  "id": "1066744245904654336",    "source": "twitter"}]';
-var wallElements = JSON.parse(wallElementsJsonData);
+Vue.prototype.$http = axios;
+
 
 //Wall element component
 Vue.component('wall-element', {
@@ -35,17 +34,32 @@ Vue.component('wall-element', {
 });
 
 // Vue app
-// '<div class="source-icon" v-bind:class="[\'source-\'+wallElement.source]"></div>' +
 var vm = new Vue({
     el: "#portfolioFeed",
     data: {
 	    version: 1.0,
-      wallElements: wallElements
+      wallElementsLoaded: false,
+      dataFeedUrl: 'http://borrazb.test',
+      wallElements: []
     },
     methods:{
         getPrettyDate: function(dateString){
             let dateObject = new Date(Date.parse(dateString));
             return dateObject.getDate()+'/'+dateObject.getMonth()+'/'+dateObject.getFullYear();
         }
-    }
+    },
+    mounted:
+      function(){
+        this.$http
+        .get(this.dataFeedUrl)
+        .then(response => {
+          this.wallElementsLoaded = true;
+          this.wallElements = response.data;
+        })
+        .catch(error => {
+          console.log(error)
+          this.wallElementsLoaded = false
+        })
+        .finally(() => this.wallElementsLoaded = false)
+      }
 });
